b0cc8b2e1e37e05537c27a525380c2ea15c643b2
Waseem Hassan <waseem.hassan119@gmail.com>
2023-06-24 21:11:54 +0200

    Update style.js

--- assets/style.js
+++ assets/style.js
@@ -36,7 +36,7 @@ const headercolor = accentcolor;
 const headerfontsize = "18pt";
 const headerdecoration = "none";
 const namecolor = accentcolor;
-const namefontsize = "23pt";
+const namefontsize = "40pt";
 
 
 // Publication properties
